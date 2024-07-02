336680d6574d7df2cabb091ac59bba6f288761f5
YapKR <124853494+YapKR@users.noreply.github.com>
2024-07-02 13:18:45 +0800

    Create Navi.js

---
+++ Navi.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('showSignup').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('loginForm').classList.add('hidden');
+    document.getElementById('signupForm').classList.remove('hidden');
+  });
+  
+  document.getElementById('showLogin').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('signupForm').classList.add('hidden');
+    document.getElementById('loginForm').classList.remove('hidden');
+  });
+
+
